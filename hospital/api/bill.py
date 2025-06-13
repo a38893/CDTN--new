b43154ae761eebb44035126cdc45669943e23f9e
nguyen hieu <nguyenhieund2303@gmail.com>
2025-06-14 00:09:57 +0700

    Update cho frontend

--- hospital/api/bill.py
+++ hospital/api/bill.py
@@ -3,9 +3,13 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from ..models import Payment
 from ..serializers import PaymentSerializer
+from rest_framework import permissions
+
 
 class PaymentListAPI(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.AllowAny] 
+
+    # permission_classes = [IsAuthenticated]
 
     def get(self, request):
         payments = Payment.objects.filter(appointment__patient_user_id=request.user).order_by('-payment_timestamp')
